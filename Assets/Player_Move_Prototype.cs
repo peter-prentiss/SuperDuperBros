635dd6a17ea29ddcf817721c4261e28459894a5d
Peter <peter.prentiss@gmail.com>
2018-11-17 09:41:07 -0600

    Create playermovement method

--- Assets/Player_Move_Prototype.cs
+++ Assets/Player_Move_Prototype.cs
@@ -2,15 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class Player_Move_Prototype : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+	public int playerSpeed = 10;
+	public bool facingRight = true;
+	public int playerJumpPower = 1250;
+	public float moveX;
 	
 	// Update is called once per frame
 	void Update () {
-		
+		PlayerMove();
+	}
+	
+	void PlayerMove () {
+		//CONTROLS
+		moveX = Input.GetAxis("Horizontal"); 
+		//ANIMATION
+		//PLAYER DIRECTION
+		if (moveX > 0.0f && facingRight == false) {
+			FlipPlayer();
+		}
+		else if (moveX < 0.0f && facingRight == true) {
+			FlipPlayer ();
+		}
+		//PHYSICS
+		gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2 (moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+	}
+	
+	void Jump () {
+		//JUMPING CODE
+	}
+	
+	void FlipPlayer () {
+	
 	}
 }
