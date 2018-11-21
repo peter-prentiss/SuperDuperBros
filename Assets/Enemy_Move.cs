8d9c59fb1eea631cc59a64511b2a61083c0700e1
Peter <peter.prentiss@gmail.com>
2018-11-21 14:43:41 -0600

    Create enemy move script

---
+++ Assets/Enemy_Move.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Move : MonoBehaviour {
+
+	public int enemySpeed;
+	public int xMoveDirection;
+
+	// Update is called once per frame
+	void Update () {
+		gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2 (xMoveDirection, 0) * enemySpeed;
+	}
+}
