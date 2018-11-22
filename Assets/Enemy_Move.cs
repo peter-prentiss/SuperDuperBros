995d35a82e054fced397203f3ade31d08d3e7969
Peter <peter.prentiss@gmail.com>
2018-11-22 14:52:01 -0600

    Enemy turns when colliding with object

--- Assets/Enemy_Move.cs
+++ Assets/Enemy_Move.cs
@@ -9,6 +9,18 @@ public class Enemy_Move : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+		RaycastHit2D hit = Physics2D.Raycast (transform.position, new Vector2 (xMoveDirection, 0));
 		gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2 (xMoveDirection, 0) * enemySpeed;
+		if (hit.distance <0.7f) {
+			Flip ();
+		}
 	}
+
+	void Flip () {
+		if (xMoveDirection < 0) {
+			xMoveDirection = -1;
+		} else {
+			xMoveDirection = 1;
+		}
+	};
 }
