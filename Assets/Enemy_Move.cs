13efa9d0116e4884e6feb1a38fae42b938076d63
Peter <peter.prentiss@gmail.com>
2018-11-23 14:56:55 -0600

    Fixed bug in enemy script flip method

--- Assets/Enemy_Move.cs
+++ Assets/Enemy_Move.cs
@@ -11,16 +11,16 @@ public class Enemy_Move : MonoBehaviour {
 	void Update () {
 		RaycastHit2D hit = Physics2D.Raycast (transform.position, new Vector2 (xMoveDirection, 0));
 		gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2 (xMoveDirection, 0) * enemySpeed;
-		if (hit.distance <0.7f) {
+		if (hit.distance < 0.7f) {
 			Flip ();
 		}
 	}
 
 	void Flip () {
-		if (xMoveDirection < 0) {
+		if (xMoveDirection > 0) {
 			xMoveDirection = -1;
 		} else {
 			xMoveDirection = 1;
 		}
-	};
+	}
 }
