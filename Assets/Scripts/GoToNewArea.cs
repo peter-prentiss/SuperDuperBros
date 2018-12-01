8dfd59fac6531546dae6e9ee6bbe595176088763
Peter <peter.prentiss@gmail.com>
2018-12-01 09:22:12 -0600

    Add clouds and warping

---
+++ Assets/Scripts/GoToNewArea.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToNewArea : MonoBehaviour {
+
+	public GameObject sp1, sp2;
+
+	void Start () {
+		sp1 = this.gameObject;
+	}
+	
+	// Update is called once per frame
+	void OnTriggerStay2D (Collider2D trig) {
+		if (Input.GetButtonDown("Jump")) {
+			trig.gameObject.transform.position = sp2.transform.position;
+		}
+	}
+}
